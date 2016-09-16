97fa8d8750973f77d7c70b57653479675dabaa6f
Farhad Shahbazi <f.shahbazi@sphericalelephant.com>
2016-09-16 14:38:34 +0200

    switch from ImageMagick to VIPS and optimize pngs afterwards

--- README.md
+++ README.md
@@ -4,16 +4,21 @@
 
 This is a one-shoot script so it is not very pretty.
 
-`image-tiler.sh` depends on `bash` and `imagemagick`
-
 ## Usage
 
 ```
-image-tiler.sh [-i input.jpg] [-o output_dir] [-l levels] [-d]
+image-tiler.sh [-i input.jpg] [-o output_dir] [-s .png] [-t 256] [-d]
 ```
 
 ## Example
 
 ```
-image-tiler.sh -i map.jpg -o ./tiles -l 5
+image-tiler.sh -i map.jpg -o tiles
 ```
+
+## Dependencies
+
+* [VIPS](http://www.vips.ecs.soton.ac.uk)
+* [GNU Parallel](https://www.gnu.org/software/parallel/)
+* [OptiPNG](http://optipng.sourceforge.net/)
+* [AdvanceCOMP](http://www.advancemame.it/comp-readme)
--- image-tiler.sh
+++ image-tiler.sh
@@ -3,22 +3,31 @@
 set -u -o pipefail
 set -E
 
+exists_in_path () {
+    command -v "$1" >/dev/null 2>&1 \
+        || { echo >&2 "I require $1 but it's not installed.  Aborting."; exit 1; }
+}
+
+for cmd in vips parallel advdef; do
+    exists_in_path "$cmd"
+done
+
 usage() {
-    echo "Usage: $0 [-i input.jpg] [-o dir] [-l levels] [-d]" 1>&2
+    echo "Usage: $0 [-i input.jpg] [-o dir] [-s .png] [-d]" 1>&2
     exit 1
 }
 
-output_dir=.
-min_zoom_size=256
+
+output_dir=tiles
+suffix=.png
 tile_size=256
 
-while getopts "i:o:l:dm:t:" opt; do
+while getopts "i:o:ds:t:" opt; do
     case ${opt} in
         d) set -x ;;
         i) input="$OPTARG" ;;
-        l) levels="$OPTARG" ;;
         o) output_dir="$OPTARG" ;;
-        m) min_zoom_size="$OPTARG" ;;
+        s) suffix="$OPTARG" ;;
         t) tile_size="$OPTARG" ;;
         *) usage ;;
     esac
@@ -26,32 +35,17 @@ done
 
 shift "$((OPTIND-1))"
 
-if [ ! -d "$output_dir" ]; then
-    echo "Output directory ${output_dir} does not exist." 1>&2
-    exit 1
-fi
-
-extension="${input##*.}"
-
-function get_dimensions() {
-    local input="$1"
-    echo $(identify -format '%w' "$input")
+function _get_new_pngs() {
+    find "$output_dir" -iname "*.png"
 }
 
-function create_tiles() {
-    local input="$1"
-    local level="$2"
-    local output_dir_lvl="${output_dir}/${level}"
-    local level_size=$(( $min_zoom_size * (2**$level) ))
+echo "Creating tiles"
+vips dzsave "$input" "$output_dir" --suffix "$suffix" --tile-size "$tile_size" --layout google
 
-    mkdir -p "$output_dir_lvl"
-    convert "$input" \
-        -resize ${level_size}x${level_size} \
-        -crop ${tile_size}x${tile_size} \
-        -set filename:tile \
-        ${output_dir_lvl}/%[fx:page.x/${tile_size}]-%[fx:page.y/${tile_size}] %[filename:tile].${extension}
-}
+echo "Optimizing pngs with optipng"
+_get_new_pngs | parallel 'optipng -quiet -o7 "{}"'
 
-for level in $(eval echo {0..$levels}) ; do
-    create_tiles "$input" "$level"
-done
+echo "Optimizing pngs with advdef"
+_get_new_pngs | parallel 'advdef --shrink-extra -z "{}"'
+
+echo "Done!"
